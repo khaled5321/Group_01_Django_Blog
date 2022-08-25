7b0cb625830cc9bda8dad095e3784afc1fffcb72
Emad Mahmoud <56219297+EmadMahmoud@users.noreply.github.com>
2022-08-26 01:01:35 +0200

    js file
    js file: landing page for now

---
+++ blog/static/js/script.js
@@ -0,0 +1,103 @@
+//                                                                  -------   LANDING PAGE   -------
+//                                                                  -------    paggination   -------
+const pages = [
+    { url: '/page1' },
+    { url: '/page2' },
+    { url: '/page3', current: true },
+    { url: '/page4' },
+    { url: '/page5' },
+]
+
+const paginate = (pages, selector) => {
+    const currentPageIndex = pages.findIndex(page => page.current)
+    const html = `
+        <ul class="pagination-list">
+            <li class="previous">
+                ${pages[0].current ? `
+                    <span>‹</span>
+                ` : `
+                    <a href="${pages[currentPageIndex - 1].url}">‹</a>
+                `}
+            </li>
+            ${pages.map((item, index) => `
+                <li${item.current ? ' class="current"' : ''}>
+                    ${item.current ? `
+                        <span>${index + 1}</span>
+                    ` : `
+                        <a href="${item.url}">${index + 1}</a>
+                    `}
+                </li> 
+            `).join('')}
+            <li class="next">
+                ${pages[pages.length - 1].current ? `
+                    <span>›</span>
+                ` : `
+                    <a href="${pages[currentPageIndex + 1].url}">›</a>
+                `}
+            </li>
+        </ul>
+    `
+
+    document.querySelector(selector).innerHTML = html
+}
+
+const paginateWithOptions = (pages, selector, options) => {
+    const {
+        showPreviousAndNext
+    } = options
+
+    const currentPageIndex = pages.findIndex(page => page.current)
+    const html = `
+        <ul class="pagination-list">
+            ${showPreviousAndNext ? `
+                <li class="previous">
+                    ${pages[0].current ? `
+                        <span>‹</span>
+                    ` : `
+                        <a href="${pages[currentPageIndex - 1].url}">‹</a>
+                    `}
+                </li>
+            ` : ''}
+            ${pages.map((item, index) => `
+                <li${item.current ? ' class="current"' : ''}>
+                    ${item.current ? `
+                        <span>${index + 1}</span>
+                    ` : `
+                        <a href="${item.url}">${index + 1}</a>
+                    `}
+                </li> 
+            `).join('')}
+            ${showPreviousAndNext ? `
+                <li class="next">
+                    ${pages[pages.length - 1].current ? `
+                        <span>›</span>
+                    ` : `
+                        <a href="${pages[currentPageIndex + 1].url}">›</a>
+                    `}
+                </li>
+            ` : ''}
+        </ul>
+    `
+
+    document.querySelector(selector).innerHTML = html
+}
+
+
+paginate(pages, '.pagination')
+
+
+
+// toggle subscribe
+const subscribe = document.querySelectorAll('.subscribe');
+let issubscribed = true;
+
+for (let i = 0; i < subscribe.length; i++) {
+
+    subscribe[i].addEventListener('click', function () {
+
+        let mode = issubscribed ? 'SUBSCRIBE' : "UNSUBSCRIBE";
+        subscribe[i].innerHTML = mode;
+        issubscribed = !issubscribed;
+    })
+
+}
