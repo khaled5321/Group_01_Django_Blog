1634becbaa6105a104a1bfe51512a312b740b488
EmadMahmoud <emadmahmoud289@gmail.com>
2022-08-31 08:36:59 +0200

    modify the comment section and added the likes count

--- blog/static/js/script.js
+++ blog/static/js/script.js
@@ -1,98 +1,16 @@
-//                                                                  -------   LANDING PAGE   -------
-//                                                                  -------    paggination   -------
-const pages = [
-    { url: '/page1' },
-    { url: '/page2' },
-    { url: '/page3', current: true },
-    { url: '/page4' },
-    { url: '/page5' },
-]
+//                          ------  post page  --------
 
-const paginate = (pages, selector) => {
-    const currentPageIndex = pages.findIndex(page => page.current)
-    const html = `
-        <ul class="pagination-list">
-            <li class="previous">
-                ${pages[0].current ? `
-                    <span>‹</span>
-                ` : `
-                    <a href="${pages[currentPageIndex - 1].url}">‹</a>
-                `}
-            </li>
-            ${pages.map((item, index) => `
-                <li${item.current ? ' class="current"' : ''}>
-                    ${item.current ? `
-                        <span>${index + 1}</span>
-                    ` : `
-                        <a href="${item.url}">${index + 1}</a>
-                    `}
-                </li> 
-            `).join('')}
-            <li class="next">
-                ${pages[pages.length - 1].current ? `
-                    <span>›</span>
-                ` : `
-                    <a href="${pages[currentPageIndex + 1].url}">›</a>
-                `}
-            </li>
-        </ul>
-    `
-
-    document.querySelector(selector).innerHTML = html
-}
-
-const paginateWithOptions = (pages, selector, options) => {
-    const {
-        showPreviousAndNext
-    } = options
-
-    const currentPageIndex = pages.findIndex(page => page.current)
-    const html = `
-        <ul class="pagination-list">
-            ${showPreviousAndNext ? `
-                <li class="previous">
-                    ${pages[0].current ? `
-                        <span>‹</span>
-                    ` : `
-                        <a href="${pages[currentPageIndex - 1].url}">‹</a>
-                    `}
-                </li>
-            ` : ''}
-            ${pages.map((item, index) => `
-                <li${item.current ? ' class="current"' : ''}>
-                    ${item.current ? `
-                        <span>${index + 1}</span>
-                    ` : `
-                        <a href="${item.url}">${index + 1}</a>
-                    `}
-                </li> 
-            `).join('')}
-            ${showPreviousAndNext ? `
-                <li class="next">
-                    ${pages[pages.length - 1].current ? `
-                        <span>›</span>
-                    ` : `
-                        <a href="${pages[currentPageIndex + 1].url}">›</a>
-                    `}
-                </li>
-            ` : ''}
-        </ul>
-    `
-
-    document.querySelector(selector).innerHTML = html
-}
-
-
-paginate(pages, '.pagination')
-
-
-
-// toggle subscribe
-const subscribe = document.querySelectorAll('.subscribe');
-subscribe.forEach((btn)=>{
-    btn.addEventListener('click', function () {
-        btn.innerText = btn.innerText==="SUBSCRIBE" ? "UNSUBSCRIBE" : "SUBSCRIBE";
-    })
-}) 
-    
+//Comment Section
+document.addEventListener(
+    "click",
+    function(event) {
+        var target = event.target;
+        var replyForm;
+        if (target.matches("[data-toggle='reply-form']")) {
+            replyForm = document.getElementById(target.getAttribute("data-target"));
+            replyForm.classList.toggle("d-none");
+        }
+    },
+    false
+);
 
